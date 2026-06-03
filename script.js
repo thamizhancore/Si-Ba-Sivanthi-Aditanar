4bf803f0dbf58e994f577ea5b144fa0d5e88395f
thamizhancore <thamizhancore@gmail.com>
2026-06-03 12:45:03 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", () => {
-  document.querySelectorAll("section").forEach(sec => {
-    if(window.scrollY > sec.offsetTop - 500){
-      sec.style.opacity = "1";
-      sec.style.transform = "translateY(0)";
-    }
-  });
-});
